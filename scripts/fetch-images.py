c85b9a9fa62fc5ac7fef75c5d9e75b3c8630680c
TempixTL <tempixtl@icloud.com>
2019-12-06 17:56:22 -0600

    download smaller images in parallel with progress

--- scripts/fetch-images.py
+++ scripts/fetch-images.py
@@ -1,41 +1,33 @@
 #!/usr/bin/env python3
 
+import multiprocessing as mp
 import os, sys
 import csv, json
 import urllib.request
+from tqdm import tqdm
 
 currentPath = os.path.dirname(os.path.realpath(__file__))
-baseImgUrl = 'https://image.tmdb.org/t/p/w500'
+baseImgUrl = 'https://image.tmdb.org/t/p/w92'
 moviesMetadataPath = os.path.join(currentPath, '../data/the-movies-dataset/movies_metadata.csv')
+
+def saveImage(posterPath):
+    posterUrl = baseImgUrl + posterPath
+    posterFilePath = os.path.join(currentPath, '../data/img/', posterPath[1:])
+
+    if not os.path.exists(posterFilePath):
+        # if img isn't already cached
+        with open(posterFilePath, 'w'): # create file if it doesn't exist
+            pass
+
+        try:
+            urllib.request.urlretrieve(posterUrl, posterFilePath)
+        except:
+            pass # I know, I'm terrible
 
 with open(moviesMetadataPath, 'r') as moviesMetadata:
     reader = csv.DictReader(moviesMetadata)
     next(reader) # trash header
-    
-    imgCount = 0
-    totalCount = 0
-    for row in reader:
-        posterPath = row['poster_path']
-        if posterPath:
-            posterUrl = baseImgUrl + posterPath
-            posterFilePath = os.path.join(currentPath, '../data/img/', posterPath[1:])
+    posterPaths = [row['poster_path'] for row in reader if row['poster_path']]
 
-            if not os.path.exists(posterFilePath):
-                # if img isn't already cached
-                with open(posterFilePath, 'w'): # create file if it doesn't exist
-                    pass
-
-                try:
-                    urllib.request.urlretrieve(posterUrl, posterFilePath)
-                    print(f'img {imgCount}: saving to', posterFilePath)
-                    imgCount += 1
-                except:
-                    pass
-            else:
-                print(f'img {imgCount}: already cached at', posterFilePath)
-                imgCount += 1
-
-        totalCount += 1
-
-    imgPct = (imgCount / totalCount) * 100
-    print(f'Found {imgCount} images out of {totalCount} ({imgPct:.2f}%)')
+    with mp.Pool() as p:
+        tqdm(p.imap_unordered(saveImage, posterPaths, 20), total=len(posterPaths))
