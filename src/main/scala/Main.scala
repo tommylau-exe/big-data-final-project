1b44c8f9db3a69c7b495ea0397ff7a749cfa7bcd
TempixTL <tempixtl@icloud.com>
2019-12-06 18:55:55 -0600

    playing with data

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,30 @@
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types.DoubleType
+import org.apache.spark.sql.functions._
+
 object Main {
   def main(args: Array[String]): Unit = {
-    println("Hello world!")
+    val spark = SparkSession.builder().master("local[*]").appName("Big Data Final Project").getOrCreate()
+    import spark.implicits._
+
+    spark.sparkContext.setLogLevel("WARN")
+
+    lazy val moviesMetadata = spark.read
+      .option("header", true)
+      .option("inferSchema", true)
+      .csv("data/the-movies-dataset/movies_metadata.csv")
+
+    lazy val posterStats = spark.read
+      .option("header", true)
+      .option("inferSchema", true)
+      .csv("data/etc/img_stats.csv")
+
+    val getPosterId = udf { (s: String) => s.drop(1).dropRight(4) }
+
+    lazy val joinedData = moviesMetadata
+      .withColumn("poster_id", getPosterId($"poster_path"))
+      .join(posterStats.withColumnRenamed("img_id", "poster_id"), "poster_id")
+
+    spark.stop()
   }
 }
