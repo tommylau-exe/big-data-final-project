7d0ecdc51894c049dc3d5235c7f4a792c7dcfacb
TempixTL <tempixtl@icloud.com>
2019-12-08 13:15:41 -0600

    Added simple random forest classifier

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,9 @@
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.types.DoubleType
 import org.apache.spark.sql.functions._
+import org.apache.spark.ml.feature.{ StringIndexer, VectorAssembler, IndexToString }
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.ml.classification.{RandomForestClassificationModel, RandomForestClassifier}
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
 
 object Main {
   def main(args: Array[String]): Unit = {
@@ -9,6 +12,7 @@ object Main {
 
     spark.sparkContext.setLogLevel("WARN")
 
+    // Raw datasets
     lazy val moviesMetadata = spark.read
       .option("header", true)
       .option("inferSchema", true)
@@ -19,11 +23,69 @@ object Main {
       .option("inferSchema", true)
       .csv("data/etc/img_stats.csv")
 
-    val getPosterId = udf { (s: String) => s.drop(1).dropRight(4) }
+    lazy val posterMovieLink = spark.read
+      .option("header", true)
+      .option("inferSchema", true)
+      .csv("data/etc/img_movie_link.csv")
 
+    lazy val moviesGenre = spark.read
+      .option("header", true)
+      .option("inferSchema", true)
+      .csv("data/etc/movies_genre.csv")
+
+    // Join all datasets
     lazy val joinedData = moviesMetadata
-      .withColumn("poster_id", getPosterId($"poster_path"))
-      .join(posterStats.withColumnRenamed("img_id", "poster_id"), "poster_id")
+      .join(posterMovieLink, "title")
+      .join(posterStats, "img_id")
+      .join(moviesGenre, "id")
+
+    val numGenres = joinedData.select("genre").distinct().count
+    println(s"Total number of genres: $numGenres")
+
+    // Set up pipeline
+    val indexer = new StringIndexer()
+      .setInputCol("genre")
+      .setOutputCol("genreIndex")
+      .fit(joinedData)
+
+    val va = new VectorAssembler()
+      .setInputCols(Array("avg_h", "avg_s", "avg_v"))
+      .setOutputCol("features")
+
+    val rf = new RandomForestClassifier()
+      .setLabelCol("genreIndex")
+      .setFeaturesCol("features")
+      .setNumTrees(10)
+
+    val unIndexer = new IndexToString()
+      .setLabels(indexer.labels)
+      .setInputCol("prediction")
+      .setOutputCol("predictionLabel")
+
+    val pipeline = new Pipeline()
+      .setStages(Array(indexer, va, rf, unIndexer))
+
+    // Split data and apply
+    val Array(trainingData, testData) = joinedData.randomSplit(Array(0.75, 0.25))
+
+    val model = pipeline.fit(trainingData)
+
+    val predictions = model.transform(testData)
+
+    // Show some predictions
+    predictions.select("features", "genre", "predictionLabel").show()
+
+    // Evaluate predictions
+    val evaluator = new MulticlassClassificationEvaluator()
+      .setLabelCol("genreIndex")
+      .setPredictionCol("prediction")
+      .setMetricName("accuracy")
+
+    val accuracy = evaluator.evaluate(predictions)
+    println(s"Classifier accuracy: ${accuracy}")
+
+    // val rfModel = model.stages(2).asInstanceOf[RandomForestClassificationModel]
+    // println(s"Learned classification forest model:\n ${rfModel.toDebugString}")
 
     spark.stop()
   }
